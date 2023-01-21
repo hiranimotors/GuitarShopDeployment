76772408700e05678441c54e9dd6633aa31b5879
omarali <o.ali94@gmail.com>
2023-01-21 16:04:30 +0000

    started work on models and fetching product images

---
+++ models/Product.model.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require("mongoose");
+
+const productSchema = new Schema(
+  {
+    productType: {
+      type: String,
+      required: true,
+    },
+    productName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    manufacturer: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    model: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    images: {
+      type: [String],
+      required: true,
+    },
+    colour: {
+      type: String,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    frets: {
+      type: Number,
+    },
+    pickups: {
+      type: [String],
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = model("Product", productSchema);
+
+module.exports = Product;
