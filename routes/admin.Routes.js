59baef6c249e6e20fb4443b23560d267eb47fde6
Craig Butler <craig.butler@jrbsolutions.co.uk>
2023-01-28 14:39:20 +0000

    created admin.routes, issue with create-product route

---
+++ routes/admin.Routes.js
@@ -0,0 +1,86 @@
+const router = require("express").Router();
+const User = require("../models/User.model");
+const Product = require("../models/Product.model");
+const {
+  isLoggedIn,
+  isLoggedOut,
+  isAdmin
+} = require("../middleware/route.guard");
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
+
+// ADMIN ROUTES
+
+
+router.get("/all-products", async (req, res, next) => {
+  try {
+    const allProducts = await Product.find();
+    console.log(allProducts);
+
+    res.render("admin/adminAllProducts", { allProducts });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:productId", (req, res, next) => {
+  try {
+    Product.findById(req.params.productId).then((individualProduct) => {
+      console.log(individualProduct);
+      res.render("admin/adminIndividualProduct", individualProduct);
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+router.get("/create-product", (req, res, next) => {
+  try {
+    res.render("adminCreateProduct");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/create-product", async (req, res, next) => {
+  try {
+    await Product.create(req.body);
+    res.redirect("/admin/all-products");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/:productId/delete", async (req, res, next) => {
+  try {
+    const id = req.params.productId
+    console.log("id is" + id)
+    await Product.findByIdAndDelete(id);
+    await res.redirect("/admin/all-products");
+  } catch (err) {
+    next(err);
+  }
+});
+
+// router.get("/admin/:productId/edit", async (req, res, next) => {
+//   try {
+//     const product = await Product.findById(req.params.productId);
+//     res.render("admin/edit-product", product);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+router.post("/admin/:productId/edit", async (req, res, next) => {
+  try {
+    await Product.findByIdAndUpdate(req.params.productId, req.body);
+    res.redirect(`/admin/${req.params.productId}`);
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+
+module.exports = router;
